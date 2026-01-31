a6830ddb4b7c7a0cba2e1ce7e75b8ad82e595a3d
GOWTHAMI REDDY SIRIGI REDDY <gowthamireddy1772@gmail.com>
2026-01-31 17:49:53 +0530

    Add files via upload

--- BankAccountSimulator.java
+++ BankAccountSimulator.java
@@ -1,43 +1,35 @@
 class BankAccount
 {
-String name;
 double balance;
-BankAccount(String name,double balance)
+BankAccount(double balance)
 {
-this.name=name;
 this.balance=balance;
-System.out.println("account created on"+name);
 }
 void deposit(double money)
 {
 balance +=money;
-System.out.println("money deposited rupees"+money);
+System.out.println("money deposited rupees "+money);
 }
 void withdraw(double cash)
 {
-if(balance<=cash)
+if(balance>=cash)
 {
 balance -=cash;
-System.out.println("money is withdrawn of rupees"+cash);
+System.out.println("money is withdrawn of rupees "+cash);
 }
 else
 {
 System.out.println("money is not enough");
 }
 }
-void display()
-{
-System.out.println("name is"+name+",balance is"+balance);
-}
 }
 public class BankAccountSimulator
 {
 public static void main(String[] args)
 {
-BankAccount ba=new BankAccount("Gowthami",1000000);
+BankAccount ba=new BankAccount(1000);
 ba.deposit(400);
 ba.withdraw(200);
-ba.display();
 }
 }
 
