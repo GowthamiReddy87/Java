2354c5a8fa398040e876f8bc9aa49f2db9449152
GOWTHAMI REDDY SIRIGI REDDY <gowthamireddy1772@gmail.com>
2026-02-01 16:55:55 +0530

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Even
+{
+    public static void main(String [] args)
+    {
+        int i=2;
+        int n=10;
+        while(i<=n)
+    {
+        System.out.println(i);
+    i=i+2;    }
+    }
+}
