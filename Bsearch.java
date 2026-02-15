bf51bc4516f4eacd1f7df0391c47fe6445c6d20a
GOWTHAMI REDDY SIRIGI REDDY <gowthamireddy1772@gmail.com>
2026-02-15 18:39:36 +0530

    Add files via upload

---
+++ Bsearch.java
@@ -0,0 +1,31 @@
+public class Bsearch
+{
+public static int search(int arr[],int target)
+{
+int start=0;
+int end=arr.length-1;
+while(start <= end)
+{
+int mid=(start+end)/2;
+if(arr[mid]==target)
+{
+return mid;
+}
+else if(arr[mid]<target)
+{
+start=mid+1;
+}
+else if(arr[mid]>target)
+{
+end=mid-1;
+}
+}
+return -1;
+}
+public static void main(String[] args)
+{
+int arr[]={1,3,7,9,11,15};
+int target=9;
+System.out.println(search(arr,target));
+}
+}
