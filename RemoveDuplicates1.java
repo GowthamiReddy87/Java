a75c2ba967a4203a27575eb1b0861ff19e978824
GOWTHAMI REDDY SIRIGI REDDY <gowthamireddy1772@gmail.com>
2026-03-30 18:12:18 +0530

    Add files via upload

---
+++ RemoveDuplicates1.java
@@ -0,0 +1,21 @@
+public class RemoveDuplicates1
+{
+public static int remove(int arr[]) {
+int j = 0;
+for(int i = 1; i < arr.length; i++) {
+if(arr[i] != arr[j]) {
+j++;
+arr[j] = arr[i];
+}
+}
+return j + 1;
+}
+public static void main(String[] args)
+{
+int arr[]={1,1,2,2,3,4,4};
+int len = remove(arr);
+for(int i = 0; i < len; i++) {
+System.out.print(arr[i] + " ");
+}
+}
+}
