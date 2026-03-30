8d58bf827c77bab990a9c4a2ade0abf1d71d9482
GOWTHAMI REDDY SIRIGI REDDY <gowthamireddy1772@gmail.com>
2026-03-30 18:11:55 +0530

    Add files via upload

---
+++ MoveZeros1.java
@@ -0,0 +1,26 @@
+public class MoveZeros1
+{
+public static void move(int arr[])
+{
+int j=0;
+for(int i=0;i<arr.length;i++)
+{
+if(arr[i]!=0)
+{
+int temp=arr[i];
+arr[i]=arr[j];
+arr[j]=temp;
+j++;
+}
+}
+for(int num:arr)
+{
+System.out.print(num+" ");
+}
+}
+public static void main(String[] args)
+{
+int arr[]={0,1,0,3,12};
+move(arr);
+}
+}
